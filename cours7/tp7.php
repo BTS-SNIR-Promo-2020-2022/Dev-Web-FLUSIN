0a3f2fd9d75296986736cf7c828b376b0673eb70
jul-fls <julienflusin3@gmail.com>
2020-12-18 11:41:01 +0100

    tp Php

---
+++ cours7/tp7.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>TP1</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <?php
+            for($i;$i<=15;$i++){
+                echo $i."<br>";
+            }
+        ?>
+    </body>
+</html>
